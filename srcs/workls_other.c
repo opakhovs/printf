af4bc6076f63113f9a98a3044790bacb9b28dbd3
opakhovs <opahovskiy@gmail.com>
2018-03-22 00:24:54 +0300

    testerc

--- srcs/workls_other.c
+++ srcs/workls_other.c
@@ -21,15 +21,13 @@ size_t	ft_wstrlen(wchar_t *str)
 		return (0);
 	while (*str)
 	{
-		if (*str <= 0x7F)
+		if (chr < (1 << 7))
 			i++;
-		else if (MB_CUR_MAX == 1 && (*str > 0x7F || *str < -128))
-			return (-1);
-		else if (*str <= 0x7FF)
+		else if (chr < (1 << 11))
 			i += 2;
-		else if (*str <= 0xFFFF)
+		else if (chr < (1 << 16))
 			i += 3;
-		else if (*str <= 0x10FFFF)
+		else if (chr < (1 << 21))
 			i += 4;
 		str++;
 	}
@@ -48,24 +46,47 @@ wchar_t	*ft_wstrnew(size_t size)
 
 void	ft_putwchar_in_char(wchar_t chr, char **str, int i)
 {
-	if (chr < (MB_CUR_MAX == 1 ? 0xFF : 0x7F))
+	unsigned int	v;
+
+	v = chr;
+	if (chr <= 0x7F)
 		*((*str) + 0 + i) = chr;
-	else if (chr < (1 << 11))
+	else if (chr <= 0x7FF)
 	{
-		*((*str) + 0 + i) = ((chr >> 6) | 0xC0);
-		*((*str) + 1 + i) = ((chr & 0x3F) | 0x80);
+		*((*str) + 0 + i) = (MASK >> 8) | (((v >> 6) << 27) >> 27);
+		*((*str) + 1 + i) = ((MASK << 24) >> 24) | ((v << 26) >> 26);
 	}
-	else if (chr < (1 << 16))
+	else if (chr <= 0xFFFF)
 	{
-		*((*str) + 0 + i) = (((chr >> 12)) | 0xE0);
-		*((*str) + 1 + i) = (((chr >> 6) & 0x3F) | 0x80);
-		*((*str) + 2 + i) = ((chr & 0x3F) | 0x80);
+		*((*str) + 0 + i) = (MASK2 >> 16) | (((v >> 12) << 28) >> 28);
+		*((*str) + 1 + i) = ((MASK2 << 16) >> 24) | ((v >> 6) << 26) >> 26;
+		*((*str) + 2 + i) = ((MASK2 << 24) >> 24) | ((v << 26) >> 26);
 	}
-	else if (chr < (1 << 21))
+	else
 	{
-		*((*str) + 0 + i) = (((chr >> 18)) | 0xF0);
-		*((*str) + 1 + i) = (((chr >> 12) & 0x3F) | 0x80);
-		*((*str) + 2 + i) = (((chr >> 6) & 0x3F) | 0x80);
-		*((*str) + 3 + i) = ((chr & 0x3F) | 0x80);
+		*((*str) + 0 + i) = (MASK3 >> 24) | (((v >> 18) << 29) >> 29);
+		*((*str) + 1 + i) = ((MASK3 << 8) >> 24) | (((v >> 12) << 26) >> 26);
+		*((*str) + 2 + i) = ((MASK3 << 16) >> 24) | (((v >> 6) << 26) >> 26);
+		*((*str) + 3 + i) = ((MASK3 << 24) >> 24) | ((v << 26) >> 26);
 	}
 }
+/*if (chr < (MB_CUR_MAX == 1 ? 0xFF : 0x7F))
+*((*str) + 0 + i) = chr;
+else if (chr < (1 << 11))
+{
+*((*str) + 0 + i) = ((chr >> 6) | 0xC0);
+*((*str) + 1 + i) = ((chr & 0x3F) | 0x80);
+}
+else if (chr < (1 << 16))
+{
+*((*str) + 0 + i) = (((chr >> 12)) | 0xE0);
+*((*str) + 1 + i) = (((chr >> 6) & 0x3F) | 0x80);
+*((*str) + 2 + i) = ((chr & 0x3F) | 0x80);
+}
+else if (chr < (1 << 21))
+{
+*((*str) + 0 + i) = (((chr >> 18)) | 0xF0);
+*((*str) + 1 + i) = (((chr >> 12) & 0x3F) | 0x80);
+*((*str) + 2 + i) = (((chr >> 6) & 0x3F) | 0x80);
+*((*str) + 3 + i) = ((chr & 0x3F) | 0x80);
+}*/
